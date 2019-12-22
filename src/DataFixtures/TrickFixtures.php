4f8067310021361dede35e19dd73efea45356e3c
Morgane <mrebindaine@hotmail.com>
2019-12-22 18:29:23 +0100

    Login form

--- src/DataFixtures/TrickFixtures.php
+++ src/DataFixtures/TrickFixtures.php
@@ -3,23 +3,43 @@
 namespace App\DataFixtures;
 
 use App\Entity\Trick;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class TrickFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct( UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create();
 
-        for ($i=0; $i<30; $i++) {
-            $trick = new Trick();
-            $trick
-                ->setName($faker->sentence(5, false))
-                ->setDescription($faker->paragraph(10));
-             $manager->persist($trick);
-        }
+//        for ($i=0; $i<30; $i++) {
+//            $trick = new Trick();
+//            $trick
+//                ->setName('test')
+//                ->setDescription('description')
+//                ->setGroup('lol');
+//             $manager->persist($trick);
+//        }
+
+        $user = new User();
+        $password = $this->encoder->encodePassword($user, 'coucou');
+        $user->setUsername('morgane')
+            ->setPassword($password)
+            ->setEmail('mrebindaine@hotmail.com');
+        $manager->persist($user);
 
         $manager->flush();
     }
