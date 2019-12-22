b3d94d4c530ec494253800d70e402836bbe65640
Morgane <mrebindaine@hotmail.com>
2019-12-22 13:54:28 +0100

    Ajax call

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,30 +7,4 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../scss/app.scss');
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
-
-$('#full-width-img').css('height', window.innerHeight)
-
-$(function(){
-    $('.arrow-down').delay( 2000 ).fadeIn( 800 )
-    setTimeout(function(){
-        let started = false
-        const addBounce = () => {
-            if(started === false) {
-                started = true
-                $('.arrow-down').addClass('animated bounce')
-                setTimeout(function(){
-                    $('.arrow-down').removeClass('animated bounce')
-                    started = false
-                }, 1000)
-            }
-        }
-
-        addBounce()
-        $('.arrow-down').hover(function(){
-            addBounce()
-        })
-    }, 4000)
-})
+require('./homepage.js');
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Repository\TrickRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -13,10 +17,20 @@ class HomeController extends AbstractController
      */
     public function index(TrickRepository $trickRepository)
     {
-        $tricks = $trickRepository->findAll();
-        dump($tricks);
+        $tricks = $trickRepository->findPaginatedTricks(10, 0);
         return $this->render('home/index.html.twig', [
             'tricks' => $tricks
         ]);
+    }
+
+    /**
+     * @Route("/ajax_request/{first}", name="ajax_request")
+     */
+    public function ajaxRequest(TrickRepository $trickRepository, int $first) {
+        $tricks = $trickRepository->findPaginatedTricks(10, $first);
+        $view = $this->renderView('home/list-tricks.html.twig', ['tricks' => $tricks]);
+        $response = new JsonResponse(['view' => $view]);
+
+        return $response;
     }
 }
