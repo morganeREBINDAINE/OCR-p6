cb53b667cea0fbc1f7aa744fe53564a178c92817
Morgane <mrebindaine@hotmail.com>
2019-12-23 13:53:40 +0100

    Mail to confirm account

--- src/Entity/Token.php
+++ src/Entity/Token.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="TokenRepository")
+ * @ORM\Entity(repositoryClass="App\Repository\TokenRepository")
  */
 class Token
 {
