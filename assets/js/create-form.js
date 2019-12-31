33a068c2338e6dd35e95652b279d7982fcc27730
Morgane <mrebindaine@hotmail.com>
2019-12-31 08:58:26 +0100

    Handle main img on create (dynamically display preview, empty input on click on buttons)

--- assets/js/create-form.js
+++ assets/js/create-form.js
@@ -1,9 +1,9 @@
 const urlRegex = new RegExp('^/creer-figure$')
 
 if (urlRegex.test(window.location.pathname)) {
-    //display image preview and video
+    //display image preview
     $('#trick_imagesFiles').on('change', (evt) => {
-        console.log(evt.target.files)
+        $('#media-images').empty()
         for (i = 0; i < evt.target.files.length; i++) {
             var reader = new FileReader();
 
@@ -12,18 +12,49 @@ if (urlRegex.test(window.location.pathname)) {
                 const image_container = $('<div class="col-sm generated"></div>')
                 image_container.append(image)
                 $('#media-images').append(image_container)
-                $('.generated').off('click')
-                $('.generated').on('click', (evt) => {
-                    document.querySelectorAll('.generated')
-                    // for(j=0;j<elem.parentNode.length;j++) {
-                    //     // if (elem.parentNode[i] == elem) //.... etc.. etc...
-                    // }
-                })
-        // console.log(document.querySelectorAll('.generated'))
             }
 
             reader.readAsDataURL(evt.target.files[i]);
         }
     })
+
+    //main_img trigger click
+    $('.trick-image-edit, .trick-image-add').on('click', (evt) => {
+        evt.preventDefault()
+        $('#trick_mainImageFile').trigger('click')
+    })
+
+    // main img display preview
+    $('#trick_mainImageFile').on('change', (evt) => {
+        if (evt.target.files.length > 0) {
+            $('.trick-image-edit, .trick-image-delete').show()
+            $('.trick-image-add').hide()
+        }
+        var reader = new FileReader();
+
+        reader.onload = function(event) {
+            $('.generated.main').remove()
+            const image = $('<div>').css('background-image', 'url('+event.target.result+')');
+            const image_container = $('<div class="col-sm generated main"></div>')
+            image_container.append(image)
+            $('#media').append(image_container)
+
+            $('.content-img').css('background-image', 'url('+event.target.result+')').addClass('has-image')
+        }
+
+        reader.readAsDataURL(evt.target.files[0]);
+    })
+
+    // empty input mainImg
+    $('.trick-image-delete').on('click', (evt) => {
+        evt.preventDefault()
+        $('#trick_mainImageFile').val('')
+        $('.generated.main').remove()
+        $('.content-img').css('background-image', 'url(/images/placehold.jpg)')
+        $('.trick-image-edit, .trick-image-delete').hide()
+        $('.trick-image-add').show()
+    })
+
+    //add video
 
 }
