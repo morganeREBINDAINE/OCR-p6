aa141d6db839ce0540d8ed6c1ed4297373890295
Morgane <mrebindaine@hotmail.com>
2020-01-01 14:58:42 +0100

    Minor fixes

--- src/Listener/ImageCacheSubscriber.php
+++ src/Listener/ImageCacheSubscriber.php
@@ -3,12 +3,9 @@
 namespace App\Listener;
 
 use App\Entity\Image;
-use App\Entity\Trick;
 use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\LifecycleEventArgs;
-use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Liip\ImagineBundle\Imagine\Cache\CacheManager;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 
 class ImageCacheSubscriber implements EventSubscriber
@@ -32,24 +29,13 @@ class ImageCacheSubscriber implements EventSubscriber
     public function getSubscribedEvents()
     {
         return [
-            'preRemove',
-            'preUpdate'
+            'preRemove'
         ];
     }
 
     public function preRemove(LifecycleEventArgs $args) {
         $image = $args->getEntity();
-        // @todo update
         if($image instanceof Image) {
-            $this->cacheManager->remove($this->uploaderHelper->asset($image, 'imageFile'));
-        }
-    }
-
-    public function preUpdate(PreUpdateEventArgs $args) {
-        $image = $args->getEntity();
-        if($image instanceof Image &&
-            ($image->getImageFile() instanceof UploadedFile || $image->getImageFile() === null)
-        ) {
             $this->cacheManager->remove($this->uploaderHelper->asset($image, 'imageFile'));
         }
     }
